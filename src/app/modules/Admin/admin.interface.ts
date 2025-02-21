d7b0aecbb7921157e7c2cf5cfb7c2d08b0cd2648
Emon Morol <ekramulhasane69@gmail.com>
2025-02-21 11:43:08 +0600

    modules created

--- src/app/modules/Admin/admin.interface.ts
+++ src/app/modules/Admin/admin.interface.ts
@@ -17,7 +17,7 @@ export type TUserName = {
   lastName: string;
 };
 
-export type TFaculty = {
+export type TAdmin = {
   id: string;
   user: Types.ObjectId;
   designation: string;
@@ -31,11 +31,9 @@ export type TFaculty = {
   presentAddress: string;
   permanentAddress: string;
   profileImg?: string;
-  academicDepartment: Types.ObjectId;
   isDeleted: boolean;
 };
 
-export interface FacultyModel extends Model<TFaculty> {
-  // eslint-disable-next-line no-unused-vars
-  isUserExists(id: string): Promise<TFaculty | null>;
+export interface AdminModel extends Model<TAdmin> {
+  isUserExists(id: string): Promise<TAdmin | null>;
 }
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -130,7 +130,7 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-  const result = await Student.findOne({ id })
+  const result = await Student.findById(id)
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
@@ -177,9 +177,9 @@ const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
     }
   }
 
-  console.log(modifiedUpdatedData);
+  // console.log(modifiedUpdatedData);
 
-  const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
+  const result = await Student.findByIdAndUpdate(id, modifiedUpdatedData,{
     new: true,
     runValidators: true,
   });
@@ -192,8 +192,8 @@ const deleteStudentFromDB = async (id: string) => {
   try {
     session.startTransaction();
 
-    const deletedStudent = await Student.findOneAndUpdate(
-      { id },
+    const deletedStudent = await Student.findByIdAndUpdate(
+      id,
       { isDeleted: true },
       { new: true, session },
     );
